93c9b099fa0bd91b8b3dc820414d3258083de7f7
Lyuzika <lyuzika@gmail.com>
2024-08-21 16:31:35 +0300

    store, actions, reducers add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,14 @@
-import { useState, useEffect } from "react";
-import SearchBox from "./SearchBox/SearchBox";
-import initialContacts from "../data/initialContacts.json";
-import ContactList from "./ContactList/ContactList";
-import ContactForm from "./ContactForm/ContactForm";
-import css from "./App.module.css";
+import { Layout } from './Layout/Layout';
+import { AppBar } from './AppBar/AppBar';
+import { TaskForm } from './TaskForm/TaskForm';
+import { TaskList } from './TaskList/TaskList';
 
-function App() {
-  const [contacts, setContacts] = useState(() => {
-    const saveContacts = localStorage.getItem("contacts");
-    return saveContacts ? JSON.parse(saveContacts) : initialContacts;
-  });
-  const [filter, setFilter] = useState("");
-
-  const addContact = (newContact) => {
-    console.log(newContact);
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
-  };
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== contactId);
-    });
-  };
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
-  console.log(contacts);
-
+export const App = () => {
   return (
-    <div className={css.container}>
-      <h1 className={css.title}>Phonebook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
-    </div>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
   );
-}
-
-export default App;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "modern-normalize"
-import App from './components/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);

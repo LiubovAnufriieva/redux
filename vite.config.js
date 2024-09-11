9bc15683a9f46a587d456610cc89f8df02791fa7
Lyuzika <lyuzika@gmail.com>
2024-09-11 08:57:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
-});
+  plugins: [react()],
+  build: {
+        sourcemap: true,
+      }
+})
 
 
 
